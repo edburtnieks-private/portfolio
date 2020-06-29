9d48caa28d2b4bc7a5d1d2308d849bc291e9203d
Edgar Burtnieks <edburtnieks@gmail.com>
2020-06-29 20:35:04 +0300

    Update title

--- src/page-components/Home/Hero/index.vue
+++ src/page-components/Home/Hero/index.vue
@@ -2,7 +2,7 @@
   <Section>
     <h1 class="hero-title">
       <div>
-        Full Stack Designer saving time by building engaging interactions
+        Full Stack Designer saving time by building easy-to-use web apps
       </div>
       <div class="hero-subtitle">
         Currently looking for new development opportunities
